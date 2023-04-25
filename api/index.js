ef577d96327daac6902d5e4b301b9ee23e155074
fellgar246 <fellgar246@gmail.com>
2023-04-25 12:10:00 -0600

    update

--- api/index.js
+++ api/index.js
@@ -20,11 +20,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const saveApiData = require("./src/controllers/saveApiData.js");
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, async() => {
+  server.listen(PORT, async() => {
     await saveApiData();
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
